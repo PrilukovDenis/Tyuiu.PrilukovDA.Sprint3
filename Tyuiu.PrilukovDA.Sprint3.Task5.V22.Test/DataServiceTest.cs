6a5edc4f4790c77e50f0d8e8eb009ba5b66d93cf
deusvela <d.prilukov@aerokod.ru>
2025-12-07 13:26:48 +0500

    Task5

---
+++ Tyuiu.PrilukovDA.Sprint3.Task5.V22.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint3.Task5.V22.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint3.Task5.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(-1.009, ds.GetSumSumSeries(2, 1, 1, 3, 12));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint3.Task5.V22/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PrilukovDA.Sprint3.Task5.V22.Lib;
+
+DataService ds = new DataService();
+
+Console.Write("Введите x ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите startValue1 ");
+int startValue1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите startValue2 ");
+int startValue2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите stopValue1 ");
+int stopValue1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите stopValue2 ");
+int stopValue2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
