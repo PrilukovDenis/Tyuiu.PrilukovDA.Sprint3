b72687809f04b896c8191c5cbc67657290153a81
deusvela <d.prilukov@aerokod.ru>
2025-12-07 13:07:17 +0500

    Task3

---
+++ Tyuiu.PrilukovDA.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Tyuiu.PrilukovDA.Sprint3.Task3.V10.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("gdff vft ", ds.DeleteCharInString("gdfppf vfppt p", 'p'));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint3.Task3.V10/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.PrilukovDA.Sprint3.Task3.V10.Lib;
+
+DataService ds = new DataService();
+
+Console.Write("Введите строку ");
+string value = Convert.ToString(Console.ReadLine());
+
+Console.Write("Введите символ ");
+char chr = Convert.ToChar(Console.ReadLine());
+
+Console.Write(ds.DeleteCharInString(value, chr));
