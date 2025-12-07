ae9342a8a1a088cc205c13c12edc3eb2e8af077c
deusvela <d.prilukov@aerokod.ru>
2025-12-07 13:16:50 +0500

    Task4

---
+++ Tyuiu.PrilukovDA.Sprint3.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint3.Task4.V10.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint3.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(0.014, ds.Calculate(-5, 5));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.PrilukovDA.Sprint3.Task4.V10.Lib;
+
+DataService ds = new DataService();
+
+Console.Write("Введите начало ");
+int startValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите конец ");
+int stopValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(ds.Calculate(startValue, stopValue));
